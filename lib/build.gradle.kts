1dda67ce7033df4411c124b28bbf52df933f5548
Brayan Oliveira <69634269+brayandso@users.noreply.github.com>
2024-12-06 15:25:31 -0300

    release: 3.0.1
    update jitpack configuration

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -66,7 +66,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.BrayanDSO"
                 artifactId = "SearchPreference"
-                version = "3.0.0"
+                version = "3.0.1"
             }
         }
     }
